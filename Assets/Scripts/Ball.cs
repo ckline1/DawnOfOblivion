9538272ad87362cfef749dbe3598e10f63244f18
obliviongamer27 <32586807+obliviongamer27@users.noreply.github.com>
2019-09-02 18:29:44 -0400

    Move and Physics
    Added movement and physics

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] bool isCollide;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        isCollide = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        isCollide = true;
+        if (collision.gameObject.tag == "Player")
+        {
+            Vector3 forceVec = new Vector3(10.0f, 500.0f, 0.0f);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(forceVec);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] float speed = 5.0f;
+    Transform currentTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTransform = transform;
+        if (Input.GetKey(KeyCode.W))
+        {
+            currentTransform.position = new Vector3(currentTransform.position.x, currentTransform.position.y, currentTransform.position.z + (speed * Time.deltaTime));
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.S))
+            {
+                currentTransform.position = new Vector3(currentTransform.position.x, currentTransform.position.y, currentTransform.position.z - (speed * Time.deltaTime));
+            }
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            currentTransform.position = new Vector3(currentTransform.position.x - (speed * Time.deltaTime), currentTransform.position.y, currentTransform.position.z);
+        } else
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                currentTransform.position = new Vector3(currentTransform.position.x + (speed * Time.deltaTime), currentTransform.position.y, currentTransform.position.z);
+            }
+        }
+        
+
+    }
+}
